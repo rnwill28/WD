f6be2ea334aacfea103fba68418ab58091a8267f
rnwill28 <wil28.lee@gmail.com>
2020-02-26 03:36:56 +0800

    web development_reOrganized

---
+++ jscript/script.js
@@ -0,0 +1,54 @@
+function valUser() {
+
+var userName = document.getElementById("userName").value;
+var psWrd = document.getElementById("password").value;
+
+	if(userName == "pusa" && psWrd == "mayor") {
+		window.location.href = "../html/resume.html";
+		document.getElementById("userName").reset();
+	} else if ( userName == "" || psWrd == "") {
+		alert("Login failed, do not leave Username or Password blank");
+	} else {
+		alert("Incorrect Username or Password");
+	}
+}
+
+function activity(){
+	window.location.href = "../html/activitypage.html";
+}
+function logOut(){
+	window.location.href = "../html/loginpage.html";
+}
+
+function Add(){
+	var num1 = parseInt(document.getElementById("num1").value);
+	var num2 = parseInt(document.getElementById("num2").value);
+	var result = num1 + num2;
+
+	document.getElementById("result").value = result;   
+}
+
+function Subtract(){
+	var num1 = parseInt(document.getElementById("num1").value);
+	var num2 = parseInt(document.getElementById("num2").value);
+	var result = num1 - num2;
+
+	document.getElementById("result").value = result;   
+	 
+}
+
+function Multiply(){
+	var num1 = parseInt(document.getElementById("num1").value);
+	var num2 = parseInt(document.getElementById("num2").value);
+	var result = num1 * num2;
+
+	document.getElementById("result").value = result;   
+}
+
+function Divide(){
+	var num1 = parseInt(document.getElementById("num1").value);
+	var num2 = parseInt(document.getElementById("num2").value);
+	var result = num1 / num2;
+
+	document.getElementById("result").value = result;   
+}	
